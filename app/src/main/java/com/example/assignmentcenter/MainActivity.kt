b9dfdd8c3218899e2bc1324d0ab6e5f07a1d17e6
Patryk <ssnickers700@gmail.com>
2023-04-24 20:49:35 +0200

    add clearing backtrack after navigating to list screen

--- app/src/main/java/com/example/assignmentcenter/MainActivity.kt
+++ app/src/main/java/com/example/assignmentcenter/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.assignmentcenter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.room.Room
 import com.example.assignmentcenter.fragments.EditFragment
 import com.example.assignmentcenter.fragments.ListFragment
@@ -23,31 +24,31 @@ class MainActivity : AppCompatActivity(), Navigable {
     }
 
     override fun navigate(to: Navigable.Destination, fragment: Fragment) {
-        supportFragmentManager.beginTransaction().apply {
-            when (to) {
-                Navigable.Destination.List -> replace(
-                    R.id.container,
-                    listFragment,
-                    listFragment.javaClass.name
-                )
-                Navigable.Destination.Add -> {
-                    replace(R.id.container, EditFragment(), EditFragment::class.java.name)
-                    addToBackStack(EditFragment::class.java.name)
-                }
-                Navigable.Destination.Preview -> {
-                    if (fragment is PreviewFragment) {
-                        replace(R.id.container, fragment)
-                        addToBackStack(PreviewFragment::class.java.name)
-                    }
-                }
-                Navigable.Destination.Edit -> {
-                    if (fragment is EditFragment) {
-                        replace(R.id.container, fragment)
-                        addToBackStack(EditFragment::class.java.name)
-                    }
+        val transaction = supportFragmentManager.beginTransaction()
+
+        when (to) {
+            Navigable.Destination.List -> {
+                supportFragmentManager.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+                transaction.replace(R.id.container, listFragment, listFragment.javaClass.name)
+            }
+            Navigable.Destination.Add -> {
+                transaction.replace(R.id.container, EditFragment(), EditFragment::class.java.name)
+                transaction.addToBackStack(EditFragment::class.java.name)
+            }
+            Navigable.Destination.Preview -> {
+                if (fragment is PreviewFragment) {
+                    transaction.replace(R.id.container, fragment)
+                    transaction.addToBackStack(PreviewFragment::class.java.name)
                 }
             }
-        }.commit()
+            Navigable.Destination.Edit -> {
+                if (fragment is EditFragment) {
+                    transaction.replace(R.id.container, fragment)
+                    transaction.addToBackStack(EditFragment::class.java.name)
+                }
+            }
+        }
+        transaction.commit()
     }
 
 }
